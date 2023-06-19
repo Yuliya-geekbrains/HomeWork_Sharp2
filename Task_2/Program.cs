cb1875a00b69ce7808afc6ba1d12281564ff1f83
Юлия <YUGG2512@gmail.com>
2023-06-19 22:43:48 +0300

    HomeWork2

---
+++ Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+// (Максимум 10000)
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+
+int number = new Random().Next(10000);
+int num1 = number % 10;
+int num2 = number / 10 % 10;
+
+if(number >= 100 && number <= 999)
+{
+    Console.Write($"В числе {number} третьей цифрой является {num1}");   
+}
+else if(number > 999)
+{
+       Console.Write($"В числе {number} третьей цифрой является {num2}");
+}
+else Console.Write("Третьей цифры нет");
+
---
+++ Task_3/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.(использовать И и ИЛИ)
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+/*Console.Write("Введите номер заданного дня недели ");
+int date = int.Parse(Console.ReadLine()!);
+
+if(date == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if(date == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if(date == 3)
+{
+    Console.WriteLine("Среда");
+}
+if(date == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if(date == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if(date == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if(date == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if(date >= 8)
+{
+    Console.WriteLine("Для этой цифры дня недели не существует");
+}*/
+
+Console.Write("Введите номер заданного дня недели ");
+int Day = int.Parse(Console.ReadLine()!);
+int weekDay1 = 6;
+int weekDay2 = 7;
+
+if(Day < weekDay1 || Day > weekDay2)
+{
+    Console.Write("Нет, это не выходной день");
+}
+else 
+{
+    Console.Write("Ура!!! Выходной!!!");
+}
