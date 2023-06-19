6558958603d3e230b79ca230790ffa888b652ef1
Yuliya <130753299+Yuliya-geekbrains@users.noreply.github.com>
2023-06-19 22:46:09 +0300

    Update Program.cs

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -5,41 +5,7 @@
 // 1 -> нет
 
 
-/*Console.Write("Введите номер заданного дня недели ");
-int date = int.Parse(Console.ReadLine()!);
 
-if(date == 1)
-{
-    Console.WriteLine("Понедельник");
-}
-if(date == 2)
-{
-    Console.WriteLine("Вторник");
-}
-if(date == 3)
-{
-    Console.WriteLine("Среда");
-}
-if(date == 4)
-{
-    Console.WriteLine("Четверг");
-}
-if(date == 5)
-{
-    Console.WriteLine("Пятница");
-}
-if(date == 6)
-{
-    Console.WriteLine("Суббота");
-}
-if(date == 7)
-{
-    Console.WriteLine("Воскресенье");
-}
-if(date >= 8)
-{
-    Console.WriteLine("Для этой цифры дня недели не существует");
-}*/
 
 Console.Write("Введите номер заданного дня недели ");
 int Day = int.Parse(Console.ReadLine()!);
